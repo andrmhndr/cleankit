f56e95fe4f115d5bb5e4de71e45668eaffe7f1eb
Budiandra YM <66835454+andrmhndr@users.noreply.github.com>
2025-06-06 08:49:16 +0700

    index exporter

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts", "src/next/index.ts", "src/react/index.ts"],
+  entry: ["src/index.ts", "src/next/index.ts", "src/react/index.ts", "src/server/index.ts"],
   dts: true,
   format: ["esm", "cjs"],
   outDir: "dist",
