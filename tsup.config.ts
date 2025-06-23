4eb346b52d2bf2aa2f76fbfbe30800809a3e8bdf
Budiandra YM <66835454+andrmhndr@users.noreply.github.com>
2025-06-24 00:36:54 +0700

    32

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,19 +1,11 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: [
-    "src/index.ts",
-    "src/next/index.ts",
-    "src/react/index.ts",
-    "src/server/index.ts",
-  ],
-  format: ["esm", "cjs"],
+  entry: ["src/index.ts", "src/next/index.ts", "src/react/index.ts", "src/server/index.ts"],
   dts: true,
+  format: ["esm", "cjs"],
   outDir: "dist",
   clean: true,
-  minify: true,
-  sourcemap: true,
-  splitting: true,
   target: "esnext",
   external: ["fs", "path", "os", "next/router", "react-router-dom"],
 });
